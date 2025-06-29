5df76d40a7aa79b34bc711f3998e51a60fe3989f
Mulheresdogame <mulheresdogame@gmail.com>
2025-06-29 13:20:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import candidaturaRoutes from './routes/candidatura.js';
 import dotenv from 'dotenv';
+
+// Carregar variáveis do .env
 dotenv.config();
 
 const app = express();
+
+// Permitir leitura de JSON
 app.use(express.json());
 
+// Definir rotas da API
 app.use('/api/candidatura', candidaturaRoutes);
 
+// Porta do servidor
 const PORT = process.env.PORT || 4000;
+
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
